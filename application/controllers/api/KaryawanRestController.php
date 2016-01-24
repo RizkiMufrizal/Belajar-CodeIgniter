8936197ec2d3e1073c7cecd6c88a325c42649459
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-24 18:58:47 +0700

    tambah karyawan controller rest api

---
+++ application/controllers/api/KaryawanRestController.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jan 24, 2016
+ * @Time 6:17:04 PM
+ * @Encoding UTF-8
+ * @Project Belajar-CodeIgniter
+ * @Package Expression package is undefined on line 12, column 15 in Templates/Scripting/PHPClass.php.
+ * 
+ */
+require APPPATH . '/libraries/REST_Controller.php';
+
+class KaryawanRestController extends REST_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Karyawan');
+    }
+
+    public function karyawans_get() {
+
+        $page = $this->get('page');
+        $size = $this->get('size');
+
+        $response = array(
+            'content' => $this->Karyawan->getKaryawans(($page - 1) * $size, $size)->result(),
+            'totalPages' => ceil($this->Karyawan->getCountKaryawan() / $size));
+
+        $this->response($response, REST_Controller::HTTP_OK);
+    }
+
+}
