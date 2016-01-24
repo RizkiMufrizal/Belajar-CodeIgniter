47d4799e17ad8ef5fb578a8f3b15f786845e5f3d
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-24 22:32:06 +0700

    tambah app angular

---
+++ assets/app/App.js
@@ -0,0 +1,46 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jan 24, 2016
+ * @Time 9:44:15 PM
+ * @Encoding UTF-8
+ * @Project Belajar-CodeIgniter
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
+ *
+ */
+
+(function() {
+  'use strict';
+  angular
+    .module('Belajar-CodeIgniter', [
+      'ui.router',
+      'oc.lazyLoad',
+      'angular-loading-bar'
+    ]).config(function($stateProvider, $urlRouterProvider, cfpLoadingBarProvider, $windowProvider) {
+
+    var baseUrl = '/' + $windowProvider.$get().location.pathname.split('/')[1];
+
+    cfpLoadingBarProvider.includeSpinner = true;
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        views: {
+          'lazyLoadView': {
+            template: '<ng-home></ng-home>'
+          }
+        },
+        resolve: {
+          loadMyCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
+            return $ocLazyLoad.load([
+              baseUrl + '/assets/app/services/UrlService.js',
+              baseUrl + '/assets/app/directives/ngHome.js'
+            ]);
+          }]
+        }
+      });
+
+  });
+})();
