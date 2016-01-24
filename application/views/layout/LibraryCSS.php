c2dd51649ac09de2fe76a9d92d20b3642d2e48eb
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-24 19:47:22 +0700

    tambah library css

---
+++ application/views/layout/LibraryCSS.php
@@ -0,0 +1,12 @@
+<!--
+
+ @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ @Since Jan 24, 2016
+ @Time 7:28:27 PM
+ @Encoding UTF-8
+ @Project Belajar-CodeIgniter
+ @Package Expression package is undefined on line 9, column 13 in Templates/Scripting/EmptyPHPWebPage.php.
+  
+-->
+
+<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
