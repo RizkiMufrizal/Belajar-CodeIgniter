84ec5e232953ddf40057bef1f48f684425d320f3
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-24 21:40:00 +0700

    tambah halaman admin

---
+++ application/views/admin/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+
+ @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ @Since Jan 24, 2016
+ @Time 9:22:06 PM
+ @Encoding UTF-8
+ @Project Belajar-CodeIgniter
+ @Package Expression package is undefined on line 9, column 13 in Templates/Scripting/EmptyPHPWebPage.php.
+  
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Admin</title>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <?php $this->load->view('layout/LibraryCSS') ?>
+
+    </head>
+    <body>
+
+        <?php $this->load->view('layout/Header') ?>
+
+        <?php $this->load->view('layout/LibraryJS') ?>
+    </body>
+</html>
+
