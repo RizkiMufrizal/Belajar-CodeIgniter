3d7f0b372e755999cbfb2023f9483b0cf78fca89
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-24 22:31:06 +0700

    tambah layout library angular js

---
+++ application/views/layout/LibraryAngularJS.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/angular.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/angular-ui-router.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/ocLazyLoad.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/loading-bar.min.js"></script>
+
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/app/App.js"></script>
