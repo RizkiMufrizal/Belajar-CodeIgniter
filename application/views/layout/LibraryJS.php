0fe388d834e2acbf335c108be362903743450ba1
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-24 22:31:20 +0700

    update layout library js

--- application/views/layout/LibraryJS.php
+++ application/views/layout/LibraryJS.php
@@ -1,13 +1,2 @@
-<!--
-
- @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
- @Since Jan 24, 2016
- @Time 7:28:35 PM
- @Encoding UTF-8
- @Project Belajar-CodeIgniter
- @Package Expression package is undefined on line 9, column 13 in Templates/Scripting/EmptyPHPWebPage.php.
-  
--->
-
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-1.12.0.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
