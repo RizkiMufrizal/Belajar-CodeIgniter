8ae2b0533a0bc7b3a5f61e9572d83ecfa706ee5e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-25 21:56:45 +0700

    update paging

--- assets/app/controllers/KaryawanController.js
+++ assets/app/controllers/KaryawanController.js
@@ -22,14 +22,67 @@
 
     karyawan.dataKaryawan = {};
     karyawan.inputKaryawan = {};
+    karyawan.paging = {
+      page: 1,
+      jumlah: 5
+    };
 
     function getKaryawans() {
-      KaryawanService.getKaryawans(1, 5).success(function(data) {
+      KaryawanService.getKaryawans(karyawan.paging.page, karyawan.paging.jumlah).success(function(data) {
         karyawan.dataKaryawan = data.content;
+        karyawan.paging.totalPages = data.totalPages;
       });
     }
 
     getKaryawans();
+
+    karyawan.getKaryawan = function(key, value) {
+      KaryawanService.getKaryawan(key, value).success(function(data) {
+        if (angular.equals(data.content, [])) {
+          alert('Data tidak tersedia');
+        } else {
+          karyawan.dataKaryawan = data.content;
+        }
+      });
+    };
+
+    karyawan.getKaryawans = function() {
+      KaryawanService.getKaryawans(1, 5).success(function(data) {
+        karyawan.dataKaryawan = data.content;
+        karyawan.paging.totalPages = data.totalPages;
+      });
+    };
+
+    //paging
+    karyawan.nextPage = function() {
+      if (karyawan.paging.page < karyawan.paging.totalPages) {
+        karyawan.paging.page++;
+        getKaryawans();
+      }
+    };
+
+    karyawan.hasNextPage = function() {
+      if (karyawan.paging.page < karyawan.paging.totalPages) {
+        return false;
+      }
+      return true;
+    };
+
+    karyawan.previousPage = function() {
+      if (karyawan.paging.page > 1) {
+        karyawan.paging.page--;
+        getKaryawans();
+      }
+    };
+
+    karyawan.hasPreviousPage = function() {
+      if (karyawan.paging.page > 1) {
+        return false;
+      }
+      return true;
+    };
+
+    //end paging
 
     karyawan.newKaryawan = function() {
       karyawan.inputKaryawan = {};
