7a68d0b79b5a2b667710b2babceb9d053fab1536
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-25 15:37:44 +0700

    update crud model karyawan

--- application/models/Karyawan.php
+++ application/models/Karyawan.php
@@ -10,6 +10,8 @@
  * @Package Expression package is undefined on line 12, column 15 in Templates/Scripting/PHPClass.php.
  * 
  */
+require APPPATH . '/libraries/Uuid.php';
+
 class Karyawan extends CI_Model {
 
     public function getKaryawans($page, $size) {
@@ -18,6 +20,35 @@ class Karyawan extends CI_Model {
 
     public function getCountKaryawan() {
         return $this->db->count_all_results('tb_karyawan', FALSE);
+    }
+
+    public function saveKaryawan($dataKaryawan) {
+        $val = array(
+            'idKaryawan' => $this->uuid->v4(),
+            'nama' => $dataKaryawan['nama'],
+            'jenisKelamin' => $dataKaryawan['jenisKelamin'],
+            'tanggalLahir' => $dataKaryawan['tanggalLahir'],
+            'alamat' => $dataKaryawan['alamat']
+        );
+        $this->db->insert('tb_karyawan', $val);
+    }
+
+    public function updateKaryawan($dataKaryawan, $idKaryawan) {
+        $val = array(
+            'nama' => $dataKaryawan['nama'],
+            'jenisKelamin' => $dataKaryawan['jenisKelamin'],
+            'tanggalLahir' => $dataKaryawan['tanggalLahir'],
+            'alamat' => $dataKaryawan['alamat']
+        );
+        $this->db->where('idKaryawan', $idKaryawan);
+        $this->db->update('tb_karyawan', $val);
+    }
+
+    public function deleteKaryawan($idKaryawan) {
+        $val = array(
+            'idKaryawan' => $idKaryawan
+        );
+        $this->db->delete('tb_karyawan', $val);
     }
 
 }
